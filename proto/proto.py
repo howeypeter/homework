cb5ea2a87ee71768f62b882abca55b004d89589f
Pete Howey <phowey@shutterfly.com>
2017-04-25 19:01:03 -0700

    adding parsing of transactions

--- proto/proto.py
+++ proto/proto.py
@@ -15,7 +15,6 @@ def parse_file(filename):
 	    byte = "0" + str(byte)
          parsedList.append(byte)
    return parsedList
-
 def output_range_ascii(begin,end,list):
    stringOfOutput = ''
    for x in range(begin,end):
@@ -30,10 +29,27 @@ def output_range_decimal(begin,end,list):
      chrctr =  int(chrctr, 16)
      stringOfOutput = stringOfOutput + str(chrctr)
    return stringOfOutput
-L = parse_file('txnlog.dat')
-print L
+def parse_transactions(list):
+  while len(list) > 0:
+    transAct = ()
+    #type 0 - debit 1 - credit 2 - autostart 3- autostop
+    transActType = int(list[0])
+    transActTime = output_range_decimal(1,5,list)
+    transActID = output_range_decimal(5,14,list)
+    print len(list)
+    del list[0:13]
+    print len(list)
+    if transActType < 2:
+      transActDollars = output_range_decimal(0,9,list)
+      del list[0:8]
 
+
+
+
+L = parse_file('txnlog.dat')
+print "Begin Headers"
 print "System: " + output_range_ascii(0,4,L)
 print "Version: " + output_range_decimal(4,5,L)
 print "Number of Records: " + output_range_decimal(5,9,L)
-
+print "Begin Transactions"
+parse_transactions(L[9::])
